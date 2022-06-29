d76569e714db2647032f4340f10e2fed3a264fe7
Mathias Roux <mathiasroux.pro@gmail.com>
2022-06-29 11:19:37 +0200

    webgl boilerplate

--- src/js/app.js
+++ src/js/app.js
@@ -1,31 +1,60 @@
 import * as THREE from 'three';
+import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js'
 
-// init
 
-const camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-camera.position.z = 1;
+export default class Sketch{
+	constructor(options){
+		this.container = options.domElement
+		this.width = this.container.offsetWidth 
+		this.height = this.container.offsetHeight 
+		this.camera = new THREE.PerspectiveCamera( 70, this.width / this.height, 0.01, 10 );
+		this.camera.position.z = 1;
 
-const scene = new THREE.Scene();
+		this.scene = new THREE.Scene();
 
-const geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
-const material = new THREE.MeshNormalMaterial();
+		this.renderer = new THREE.WebGLRenderer( { antialias: true } );
+		this.renderer.setPixelRatio(window.devicePixelRatio)
+		this.container.appendChild(this.renderer.domElement)
+		this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
-const mesh = new THREE.Mesh( geometry, material );
-scene.add( mesh );
+		this.time = 0 
+		this.resize()
+		this.addObjects()
+		this.render()
+		this.setupResize()
+	}
 
-const renderer = new THREE.WebGLRenderer( { antialias: true } );
-renderer.setSize( window.innerWidth, window.innerHeight );
-renderer.setAnimationLoop( animation );
-let container = document.getElementById('container')
-container.appendChild( renderer.domElement );
+	resize(){
+		this.width = this.container.offsetWidth
+		this.height = this.container.offsetHeight
+		this.renderer.setSize( this.width, this.height )
+		this.camera.aspect = this. width / this.height
+		this.camera.updateProjectionMatrix()
+	}
 
-// animation
+	setupResize(){
+		window.addEventListener('resize', this.resize.bind(this))
+	}
 
-function animation( time ) {
+	addObjects(){
+		this.geometry = new THREE.BoxGeometry( 0.2, 0.2, 0.2 );
+		this.material = new THREE.MeshNormalMaterial();
+		this.mesh = new THREE.Mesh( this.geometry, this.material );
+		this.scene.add( this.mesh );
+	}
 
-	mesh.rotation.x = time / 2000;
-	mesh.rotation.y = time / 1000;
+	render(){
+		this.time += 0.05
+		this.mesh.rotation.x = this.time / 2000;
+		this.mesh.rotation.y = this.time / 1000;
 
-	renderer.render( scene, camera );
+		this.renderer.render( this.scene, this.camera );
 
+		requestAnimationFrame(this.render.bind(this))
+	}
 }
+
+
+new Sketch({
+	domElement: document.getElementById('container')
+})
